10212ab7f04b5b240e82235fb5a1976d8dca8326
sam.medley <medleykupps@outlook.com>
2016-04-15 22:39:43 +1000

    Adding xUnit unit test project and simple fixture

---
+++ Src/Tests/LiftSimulator.Core.Tests/XUnitTestFixture.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LiftSimulator.Core.Tests
+{    
+    public class XUnitTestFixture
+    {
+        [Fact]
+        public void TestXUnit()
+        {
+            Assert.True(true);
+            Assert.False(false);
+        }
+    }
+}
